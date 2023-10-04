318f2d0da72a11b5a3b571ec401e8b120593eff4
edwar107 <edwar107@go.stockton.edu>
2023-10-03 21:03:06 -0400

    commit changes Homework 2

--- HW1.py
+++ HW1.py
@@ -45,17 +45,18 @@ print("My favorite animal is "+ myAnimal +")
 # Question 4:
 # Calculate the following and print the result:
 # 2 + 2 
+print(2+2)
 
 # 3 * 4
+print(3 * 4)
 
 # 5 - 6
-
+print(5 - 6)
 # 8 / 2
-
+print(8 / 2)
 # Question 5:
 # Create a variable called "num1" and set it equal to 2
 num1 = 2
-
 # Create a variable called "num2" and set it equal to 3
 num2 = 3
 
@@ -67,36 +68,43 @@ num4 = 5
 
 # Calculate the following and print the result:
 # num1 + num2
-
+print(num1 + num2)
 # num3 * num4
-
+print(num3 + num4)
 # num4 - num1
-
+print(num4 - num1)
 # num2 / num1
-
+print(num2 / num1)
 # Question 6: Write a program that asks the user for their name and then prints the following:
 
 # Hello, <name>. Please enter three numbers.
-
+print( Hello, <name>)
 # The program should then ask the user for three numbers (floats) and print the following:
 
 
 # 1. The sum of the three numbers is <sum>
-
+print(The sum of the three numbers is <sum>)
 # 2. The product of the three numbers is <product>
-
+print(The product of the three numbers is <sum>)
 # 3. round the three numbers to the nearest integer and print the sum of the three rounded numbers divided by 3 
-
+print(round the three numbers to the nearest integer and print the sum of the three eounded numbers divided by 3)
 # 4. The average of the three numbers is <average>
-
+print(The average of the three numbers is <average>)
 # Question 7: Ask the user for an input of a symbol (in the example its *)     
 # Print a diamond of the symbol that looks like the following. Include the spaces and the | character. 
 # Hint: the print("symbol", end="") with \t and \n characters will be useful here.
 
 #    *     |
+print("*", end="")
 #   ***    |
+print("***", end="")
 #  *****   |
+print("*****", end="")
 # *******  |
+print("*******, =")
 #  *****   |
+print("*****", end="")
 #   ***    |
+print("***", end="")
 #    *     |
+print("*", end="")
