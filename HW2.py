fef6677b32fac1ed1a79c688bebfc2a3dea4cd4d
edwar107 <edwar107@go.stockton.edu>
2023-11-15 10:59:50 -0500

    commit changes

--- HW2.py
+++ HW2.py
@@ -1,32 +1,65 @@
 # HW2.py
-# Author:
+# Author: Terrisha Edwards
 
 
 # Question 1:
 # Write some code that prompts the user for their age. Depending on the input:
-
 # If the age is less than 13, print "You are a child."
+input_number = int(input("Enter a age: "))
+if input_number < 13: 
+    print("negative")
+    print("You are a child")
 # If the age is between 13 and 19, print "You are a teenager."
+input_number = int(input("Enter a age: "))
+age = int(intput("Enter your age: "))
+if 13 < age < 19: 
+    print("Your a teenager")
+else:
+    print("Your not a teenager")
 # If the age is 20 or older, print "You are an adult."
-
+input_number = int(input("Enter a age "))
+if input_number > 20: 
+    print("You are an adult")
 
 # Question 2:
 # Write some code to display the following pattern using a for or while loop:
 # 1
+for i in range(1):
+    print("1")
 # 12
+for i in range(12):
+    print ("12")
 # 123
+for i in range(123):
+    print("123")
 # 1234
+for i in range(1234):
+    print("1234")
 # 12345
+for i in range(12345):
+    print("12345")
 
 # Question 3:
 # Write a Python program that prompts the user to input 10 numbers. After all the numbers are inputted, the program should display:
 
 # The highest number.
+for i in range(10):
+    input_number = int(input("Enter a number" ))
+    sum+= input_number
+    highest = max + 10
 # The lowest number.
+input_number = int(int("Enter a number "))
+sum+= input_number
+lowest = min
 # The average of all the numbers.
-
+for i in range (10)
+input_number = int(input("Enter a number "))
+sum += input_number
+average = sum / 10 
+print(average)
 # Question 4:
 # Vowel Counter - Write some code that prompts the user to enter a string. The program should then display the number of vowels in the string. IE. If the user enters "Hello World", the program should display 3.
 # the vowels are a, e, i, o, u
+input_number = input("Enter a string")
+vowel_counter = ("a, e, i, o,u") 
 # Hint: convert the string to lowercase and use a for loop with a counter variable and an if statement
-
